1db62d89b4a7e1d62bd32e18b6eb1cccd5cb44cb
sine9o <sine9o@LAPTOP-JCIEVBAJ>
2025-05-07 23:07:24 +0800

    添加项目文件。

---
+++ QuickStart/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Hardcodet.Wpf.TaskbarNotification;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace QuickStart
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static TaskbarIcon taskBarIcon;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            taskBarIcon = (TaskbarIcon)FindResource("Taskbar");
+        }
+    }
+
+}
